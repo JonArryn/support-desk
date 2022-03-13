9b5dfabfffa3d8d3ad07e6bff2f042725c4550a1
JonArryn <dumpster@lordofthevale.com>
2022-03-13 09:44:38 -0700

    implement private route and authentication status custom hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+
+// hooks
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// components
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+
+// pages
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import NewTicket from "./pages/NewTicket";
 
 function App() {
   return (
@@ -17,6 +25,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/new-ticket" element={<PrivateRoute />}>
+              <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
